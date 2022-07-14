612a4124630f600a9b9b33b7c3743f70dc07ecf7
Caio Zanchetta Reis <caionohamachi@gmail.com>
2022-07-14 15:43:12 -0300

    Resolve Attemp1

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
 		String msg = "Vendedor "+ sale.getSellerName() + " foi destaque em " + date
-				+  " com um total de R$" + String.format("%.2f", sale.getAmount());
+				+  " com um total de R$" + String.format("#,##0.00", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
