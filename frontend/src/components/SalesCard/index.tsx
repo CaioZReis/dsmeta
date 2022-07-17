520065b7af032649f97e443b9a5f5fe25adc3326
Caio Zanchetta Reis <caionohamachi@gmail.com>
2022-07-17 14:25:55 -0300

    Mudança no tipo da sale

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -70,7 +70,7 @@ function SalesCard() {
                                 <tr key={sale.id}>
                                     <td className="show992">{sale.id}</td>
                                     <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
-                                    <td>{sale.sellerName}</td>
+                                    <td>{sale.sellerName}</td>  
                                     <td className="show992">{sale.visited}</td>
                                     <td className="show992">{sale.deals}</td>
                                     <td>R$ {sale.amount.toFixed(2)}</td>
